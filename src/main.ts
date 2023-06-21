0d30da3381a678981f69fd8a561e3bd69c040414
Maria Teterina <teterina_m_o@rambler.ru>
2023-06-22 00:19:47 +0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { handleMarkerDetection } from './marker';
 import { Server } from 'socket.io';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('D:\\Program Files\\OpenSSL-Win64\\bin\\private.key'),
-    cert: fs.readFileSync('D:\\Program Files\\OpenSSL-Win64\\bin\\certificate.crt'),
-  };
-
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    httpsOptions,
-  });
-
-  // Подключение директории для статических файлов
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const port = process.env.PORT || 3000;
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  // Подключение к серверу сокетов
   const server = app.getHttpServer();
   const io = new Server(server);
-
-  // Обработка события считывания маркерного изображения
-  handleMarkerDetection(io);
-  const port = process.env.PORT || 443;
   await app.listen(port);
   Logger.log(`Server is running on https://localhost:${port}`);
 }
-
-bootstrap();
